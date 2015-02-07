2572f543002448011f1e7ab0ab1989adea44e76f
Mohammed Ali <mohammed.ali.cs@gmail.com>
2015-02-07 12:04:38 +0200

    first commit

---
+++ src/main/java/poc/portlet/helloportlet/HelloPortlet.java
@@ -0,0 +1,25 @@
+package poc.portlet.helloportlet;
+
+import java.io.IOException;
+
+import javax.portlet.GenericPortlet;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloPortlet extends GenericPortlet {
+
+	@Override
+	protected void doView(RenderRequest request, RenderResponse response)
+			throws PortletException, IOException {
+		// TODO Auto-generated method stub
+//		super.doView(request, response);
+		response.setContentType("text/html");
+		response.getWriter().println("Hello Portlet body!!");
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+
+<html>
+  <head>
+    <script src="js/angular.min.js"></script>
+  </head>
+  <body>
+    <div>
+      <h1>Hello 1st portlet</h1>
+    </div>
+  </body>
+</html>
